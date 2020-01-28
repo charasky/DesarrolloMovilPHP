5e3eedd54cf5730e5139ef4d6b89aaa2d4de6ac7
xtatick <brendasanchez9310@gmail.com>
2020-01-28 18:37:04 -0300

    restorePassword en proceso

---
+++ v1/restorePassword.php
@@ -0,0 +1,45 @@
+
+<?php 
+//en proceso
+require_once '../includes/DbOperations.php';
+
+$response = array(); 
+
+if($_SERVER['REQUEST_METHOD']=='POST'){
+	if(
+		isset($_POST['usu_usuario']) and 
+			isset($_POST['usu_password'])){
+
+		$db = new DbOperations();
+		
+		$result = $db->createUser($_POST['usu_usuario'],
+					$_POST['usu_password'],
+					$_POST['usu_nombres'],
+					$_POST['usu_apellidos'],
+					$_POST['usu_asamblea']
+					);
+		if($result == 1){
+			$response['error'] = false;
+			$response['message'] = "Registro exitoso.";
+		}elseif($result == 2){
+			$response['error'] = true;
+			$response['message'] = "Algun error ocurrio intente denuevo.";
+		}elseif($result == 3){
+			$response['error'] = true;
+			$response['message'] = "La Asamblea ingresada no existe.";
+			$response['existe'] = "asamblea";
+		}elseif($result == 0){
+			$response['error'] = true;
+			$response['message'] = "El email ingresado ya existe, ingrese otro.";
+			$response['existe'] = "email";
+		}
+	}else{
+		$response['error'] = true;
+		$response['message'] = "required fields are missing";	
+	}
+}else{
+	$response['error'] = true; 
+	$response['message'] = "Invalid Request";
+}
+
+echo json_encode($response);
