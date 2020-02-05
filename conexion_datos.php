0fbe66e6a51051f362c144ef1bbcf2d9c0966c91
xtatick <brendasanchez9310@gmail.com>
2020-02-05 15:13:31 -0300

    modificado acceso a database

--- conexion_datos.php
+++ conexion_datos.php
@@ -1,9 +1,8 @@
 <?php
-error_reporting(E_ALL ^ E_NOTICE);
 $hostname='localhost';
 $database='datos';
-$username='root';
-$password='';
+$username='test';
+$password='password';
 
 $conexion=new mysqli($hostname,$username,$password,$database);
 if($conexion->connect_errno){
