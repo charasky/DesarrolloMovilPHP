5a205a9c1c8607874da94584775fe5d88a8a7a22
charasky <maurojaviersilva@gmail.com>
2020-02-24 12:37:37 -0300

    cambios php DbReporte

--- includes/DbReporte.php
+++ includes/DbReporte.php
@@ -37,70 +37,105 @@
  		   	return $datos;
 		}
 
-		public function allanamiento($allanamiento1, $allanamiento2, $allanamiento3, $allanamiento4, $allanamiento5, $allanamiento6, $allanamiento7, $allanamiento8){
+		public function allanamiento($usu_orden_allanamiento, $usu_agresion_allanamiento, $usu_pertenencias_allanamiento, $usu_omision_pertenencias, $usu_detenidos_allanamiento, $usu_duracion_allanamiento, $usu_esposados, $usu_posicion_allanamiento){
+			$insert_allanamiento="insert into allanamiento values(NULL,'".$usu_orden_allanamiento."','".$usu_agresion_allanamiento."','".$usu_pertenencias_allanamiento."','".$usu_omision_pertenencias."','".$usu_detenidos_allanamiento."','".$usu_duracion_allanamiento."','".$usu_esposados."','".$usu_posicion_allanamiento."')";
+			mysqli_query($this->con3,$insert_allanamiento) or die (mysqli_error($this->con3));
+
+			/*
 			$stmt = $this->con3->prepare("INSERT INTO `allanamiento` (`id`, `usu_orden_allanamiento`, `usu_agresion_allanamiento`, `usu_pertenencias_allanamiento`, `usu_omision_pertenencias`, `usu_detenidos_allanamiento`, `usu_duracion_allanamiento`, `usu_esposados`, `usu_posicion_allanamiento`) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?);");	
 			$stmt->bind_param("ssssssss",$allanamiento1, $allanamiento2, $allanamiento3, $allanamiento4, $allanamiento5, $allanamiento6, $allanamiento7, $allanamiento8);
-			$stmt->execute();	
+			$stmt->execute();	*/
 		}
 
-		public function prodecimiento($procedimiento1, $procedimiento2, $procedimiento3){
+		public function prodecimiento($usu_motivo_procedimiento, $usu_maltratos, $usu_lesiones){
+			$insert_caracteristicas_procedimiento="insert into caracteristicas_procedimiento values(NULL,'".$usu_motivo_procedimiento."','".$usu_maltratos."','".$usu_lesiones."')";
+			mysqli_query($this->con3,$insert_caracteristicas_procedimiento) or die (mysqli_error($this->con3));
+			/*
 			$stmt = $this->con3->prepare("INSERT INTO `caracteristicas_procedimiento` (`id`, `usu_motivo_procedimiento`, `usu_maltratos`, `usu_lesiones`) VALUES (NULL, ?, ?, ?);");	
 			$stmt->bind_param("sss",$procedimiento1, $procedimiento2, $procedimiento3);
-			$stmt->execute();	
+			$stmt->execute();	*/
 		}
 
-		public function entrevistado($parentesco1){
+		public function entrevistado($usu_parentesco_entrevistado){
+			$insert_entrevistado="insert into entrevistado values(NULL,'".$usu_parentesco_entrevistado."')";
+			mysqli_query($this->con3,$insert_entrevistado) or die (mysqli_error($this->con3));
+			/*
 			$stmt = $this->con3->prepare("INSERT INTO `entrevistado` (`id`, `usu_parentesco_entrevistado`) VALUES (NULL, ?);");	
 			$stmt->bind_param("s",$parentesco1);
-			$stmt->execute();	
+			$stmt->execute();	*/
 		}
 
-		public function entrevistador($entrevistador1, $entrevistador2, $entrevistador3, $entrevistador4){
+		public function entrevistador($usu_nombre, $usu_apellido, $usu_asamblea, $usu_fecha){
+			$insert_entrevistador="insert into entrevistador values(NULL,'".$usu_nombre."','".$usu_apellido."','".$usu_asamblea."','".$usu_fecha."')";
+			mysqli_query($this->con3,$insert_entrevistador) or die (mysqli_error($this->con3));
+			
+			/*
 			$stmt = $this->con3->prepare("INSERT INTO `entrevistador` (`id`, `usu_nombre`, `usu_apellido`, `usu_asamblea`, `usu_fecha`) VALUES (NULL, ?, ?, ?, ?);");	
 			$stmt->bind_param("ssss",$entrevistador1, $entrevistador2, $entrevistador3, $entrevistador4);
-			$stmt->execute();	
+			$stmt->execute();	*/
 		}
 
-		public function fuerzasIntervinientes($fuerzas1, $fuerzas2, $fuerzas3, $fuerzas4, $fuerzas5, $fuerzas6, $fuerzas7, $fuerzas8){
+		public function fuerzasIntervinientes($usu_fuerzas_intervinientes, $usu_cantidad_agentes, $usu_nombres_agentes, $usu_apodos, $usu_cantidad_vehiculos, $usu_num_movil, $usu_dominio, $usu_conducta_agentes){
+			$insert_fuerzas_intervinientes="insert into fuerzas_intervinientes values(NULL,'".$usu_fuerzas_intervinientes."','".$usu_cantidad_agentes."','".$usu_nombres_agentes."','".$usu_apodos."','".$usu_cantidad_vehiculos."','".$usu_num_movil."','".$usu_dominio."','".$usu_conducta_agentes."')";
+			mysqli_query($this->con3,$insert_fuerzas_intervinientes) or die (mysqli_error($this->con3));
+			/*
 			$stmt = $this->con3->prepare("INSERT INTO `fuerzas_intervinientes` (`id`, `usu_fuerzas_intervinientes`, `usu_cantidad_agentes`, `usu_nombres_agentes`, `usu_apodos`, `usu_cantidad_vehiculos`, `usu_num_movil`, `usu_dominio`, `usu_conducta_agentes`) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?);");	
 			$stmt->bind_param("ssssssss",$fuerzas1, $fuerzas2, $fuerzas3, $fuerzas4, $fuerzas5, $fuerzas6, $fuerzas7, $fuerzas8);
-			$stmt->execute();	
+			$stmt->execute();	*/
 		}
 
-		public function hechoPolicial($hecho1, $hecho2, $hecho3, $hecho4, $hecho5, $hecho6, $hecho7){
+		public function hechoPolicial($usu_dia_hecho, $usu_hora_hecho, $usu_ubicacion_hecho, $usu_cuantos_acompañan, $usu_cual_lugar, $usu_provincia_hecho, $usu_pais_hecho){
+			$insert_hecho_policial="insert into hecho_policial values(NULL,'".$usu_dia_hecho."','".$usu_hora_hecho."','".$usu_ubicacion_hecho."','".$usu_cuantos_acompañan."','".$usu_cual_lugar."','".$usu_provincia_hecho."','".$usu_pais_hecho."')";
+			mysqli_query($this->con3,$insert_hecho_policial) or die (mysqli_error($this->con3));
+			/*
 			$stmt = $this->con3->prepare("INSERT INTO `hecho_policial` (`id`, `usu_dia_hecho`, `usu_fecha_hecho`, `usu_ubicacion_hecho`, `usu_cuantos_acompañan`, `usu_cual_lugar`, `usu_provincia_hecho`, `usu_pais_hecho`) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?);");	
 			$stmt->bind_param("sssssss",$hecho1, $hecho2, $hecho3, $hecho4, $hecho5, $hecho6, $hecho7);
-			$stmt->execute();	
+			$stmt->execute();*/	
 		}
 
-		public function modalidadDetencion($modalidad1, $modalidad2){
+		public function modalidadDetencion($usu_posicion_detenido, $usu_tiempo_detenido){
+			$insert_modalidad_detencion="insert into modalidad_detencion values(NULL,'".$usu_posicion_detenido."','".$usu_tiempo_detenido."')";
+			mysqli_query($this->con3,$insert_modalidad_detencion) or die (mysqli_error($this->con3));
+			/*
 			$stmt = $this->con3->prepare("INSERT INTO `modalidad_detencion` (`id`, `usu_posicion_detenido`, `usu_tiempo_detenido`) VALUES (NULL, ?, ?);");	
 			$stmt->bind_param("ss",$modalidad1, $modalidad2);
-			$stmt->execute();	
+			$stmt->execute();	*/
 		}
 
-		public function omisionActuar($omision1, $omision2, $omision3, $omision4, $omision5){
+		public function omisionActuar($usu_medios_de_asistencia, $usu_a_quien_asistencia, $usu_denuncia_rechazada, $usu_violentado, $usu_denuncia_final){
+			$insert_omision_actuar="insert into omision_actuar values(NULL,'".$usu_medios_de_asistencia."','".$usu_a_quien_asistencia."','".$usu_denuncia_rechazada."','".$usu_violentado."','".$usu_denuncia_final."')";
+			mysqli_query($this->con3,$insert_omision_actuar) or die (mysqli_error($this->con3));
+			/*
 			$stmt = $this->con3->prepare("INSERT INTO `omision_actuar` (`id`, `usu_medios_de_asistencia`, `usu_a_quien_asistencia`, `usu_denuncia_rechazada`, `usu_violentado`, `usu_denuncia_final`) VALUES (NULL, ?, ?, ?, ?, ?);");	
 			$stmt->bind_param("sssss",$omision1, $omision2, $omision3, $omision4, $omision5);
-			$stmt->execute();	
+			$stmt->execute();	*/
 		}
 
-		public function resultadoInvestigacion($resultado1, $resultado2){
+		public function resultadoInvestigacion($usu_resultado_investigacion, $usu_trabajan_los_oficiales){
+			$insert_resultado_investigacion="insert into resultado_investigacion values(NULL,'".$usu_resultado_investigacion."','".$usu_trabajan_los_oficiales."')";
+			mysqli_query($this->con3,$insert_resultado_investigacion) or die (mysqli_error($this->con3));
+			/*
 			$stmt = $this->con3->prepare("INSERT INTO `resultado_investigacion` (`id`, `usu_resultado_investigacion`, `usu_trabajan_los_oficiales`) VALUES (NULL, ?, ?);");	
 			$stmt->bind_param("ss",$resultado1, $resultado2);
-			$stmt->execute();	
+			$stmt->execute();	*/
 		}
 
-		public function traslado($traslado1, $traslado2, $traslado3){
+		public function traslado($usu_traslado, $usu_comisaria, $usu_esposado){
+			$insert_traslado="insert into traslado values(NULL,'".$usu_traslado."','".$usu_comisaria."','".$usu_esposado."')";
+			mysqli_query($this->con3,$insert_traslado) or die (mysqli_error($this->con3));
+			/*
 			$stmt = $this->con3->prepare("INSERT INTO `traslado` (`id`, `usu_traslado`, `usu_comisaria`, `usu_esposado`) VALUES (NULL, ?, ?, ?);");	
 			$stmt->bind_param("sss",$traslado1, $traslado2, $traslado3);
-			$stmt->execute();	
+			$stmt->execute();	*/
 		}
 
-		public function victima($victima1, $victima2, $victima3, $victima4, $victima5, $victima6, $victima7, $victima8, $victima9){
+		public function victima($usu_nombre_victima, $usu_apellido_victima, $usu_genero_victima, $usu_edad_victima, $usu_nacionalidad_victima, $usu_documento_victima, $usu_direccion_victima, $usu_barrio_victima, $usu_telefono_victima){
+			$insert_victima="insert into victima values(NULL,'".$usu_nombre_victima."','".$usu_apellido_victima."','".$usu_genero_victima."','".$usu_edad_victima."','".$usu_nacionalidad_victima."','".$usu_documento_victima."','".$usu_direccion_victima."','".$usu_barrio_victima."','".$usu_telefono_victima."')";
+			mysqli_query($this->con3,$insert_victima) or die (mysqli_error($this->con3));
+			/*
 			$stmt = $this->con3->prepare("INSERT INTO `victima` (`id`, `usu_nombre_victima`, `usu_apellido_victima`, `usu_genero_victima`, `usu_edad_victima`, `usu_nacionalidad_victima`, `usu_documento_victima`, `usu_direccion_victima`, `usu_barrio_victima`, `usu_telefono_victima`) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?);");	
 			$stmt->bind_param("sssssssss",$victima1, $victima2, $victima3, $victima4, $victima5, $victima6, $victima7, $victima8, $victima9);
-			$stmt->execute();	
+			$stmt->execute();	*/
 		}
 
 		//para que pueda verse en buscar
--- v1/registerReporte.php
+++ v1/registerReporte.php
@@ -9,8 +9,8 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
 		isset($_POST['horaReporte']) and 
 			isset($_POST['reporte'])){
 
-		$fecha=$_POST['fechaReporte'];
-		$hora=$_POST['horaReporte'];
+		$fecha=$_POST['fechaReporte'] . ' ' . $_POST['horaReporte'];
+		//$hora=;
 		$obj = json_decode($_POST['reporte']);
 
 		$db = new DbReporte();
@@ -18,13 +18,8 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
 		//echo $obj->{'0'};
 		//falta agregar hora en java y tabla
 		$db->registrarReporte($obj->{'usu_pais_hecho'}, $obj->{'usu_provincia_hecho'}, $fecha, $obj->{'usu_nombre_victima'}, $obj->{'usu_apellido_victima'}, $obj->{'usu_asamblea'});
-		
-			//verificar por que no se crean  en tabla :v
-		//$db->allanamiento($obj->{'usu_orden_allanamiento'}, $obj->{'usu_agresion_allanamiento'}, $obj->{'usu_pertenencias_allanamiento'}, $obj->{'usu_omision_pertenencias'}, $obj->{'usu_detenidos_allanamiento'}, $obj->{'usu_duracion_allanamiento'}, $obj->{'usu_esposados'}, $obj->{'usu_posicion_allanamiento'});
-		
-		//revisar procedimiento parte lesiones no guarda la accion del boton no , sacando eso funciona
+		$db->allanamiento($obj->{'usu_orden_allanamiento'}, $obj->{'usu_agresion_allanamiento'}, $obj->{'usu_pertenencias_allanamiento'}, $obj->{'usu_omision_pertenencias'}, $obj->{'usu_detenidos_allanamiento'}, $obj->{'usu_duracion_allanamiento'}, $obj->{'usu_esposados'}, $obj->{'usu_posicion_allanamiento'});
 		$db->prodecimiento($obj->{'usu_motivo_procedimiento'}, $obj->{'usu_maltratos'}, $obj->{'usu_lesiones'});
-		 //revisar parte denuncia em reṕorte  botones si , no . no sabe, sacando eso funciona
 		$db->entrevistado($obj->{'usu_parentesco_entrevistado'});
 		$db->entrevistador($obj->{'usu_nombre'}, $obj->{'usu_apellido'}, $obj->{'usu_asamblea'}, $obj->{'usu_fecha'});
 		$db->fuerzasIntervinientes($obj->{'usu_fuerzas_intervinientes'}, $obj->{'usu_cantidad_agentes'},$obj->{'usu_nombres_agentes'}, $obj->{'usu_apodos'}, $obj->{'usu_cantidad_vehiculos'}, $obj->{'usu_num_movil'}, $obj->{'usu_dominio'}, $obj->{'usu_conducta_agentes'});
@@ -35,8 +30,10 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
 		$db->traslado($obj->{'usu_traslado'}, $obj->{'usu_comisaria'}, $obj->{'usu_esposado'});
 		$db->victima($obj->{'usu_nombre_victima'}, $obj->{'usu_apellido_victima'}, $obj->{'usu_genero_victima'}, $obj->{'usu_edad_victima'}, $obj->{'usu_nacionalidad_victima'}, $obj->{'usu_documento_victima'}, $obj->{'usu_direccion_victima'}, $obj->{'usu_barrio_victima'}, $obj->{'usu_telefono_victima'});
 		
+
 		$response['error'] = false;
 		$response['message'] = "Se ha guardado correctamente.";
+		$response['validate'] = true;
 	}else{
 		$response['error'] = true;
 		$response['message'] = "Required fields are missing";	
