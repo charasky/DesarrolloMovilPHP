032a0ba7600a475775fc0ebcf60f16603eae7640
xtatick <brendasanchez9310@gmail.com>
2020-03-12 22:17:23 -0300

    conseguirReporte utf8_encode

--- v1/conseguirReporte.php
+++ v1/conseguirReporte.php
@@ -27,6 +27,19 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
 	}
 }
 
-echo (json_encode($response));
+function utf8ize($d) {
+    if (is_array($d)) {
+        foreach ($d as $k => $v) {
+            $d[$k] = utf8ize($v);
+        }
+    } else if (is_string ($d)) {
+        return utf8_encode($d);
+    }
+    return $d;
+}
+
+echo json_encode(utf8ize($response));
+
+//echo (json_encode($response));
 
 ?>
